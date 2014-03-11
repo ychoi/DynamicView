c63048407cb334e721ee48444c645db582fc41b8
Young Choi <ychoi927@gmail.com>
2014-03-11 12:15:12 -0700

    Initial Commit

---
+++ DynamicView/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  DynamicView
+//
+//  Created by Young Choi on 3/11/14.
+//  Copyright (c) 2014 Young Choi. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
